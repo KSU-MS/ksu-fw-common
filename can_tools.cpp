9c1b81483936d54d7c6b07104daa0387296ed413
ChanceCtrl <>
2025-03-25 18:54:42 -0400

    added generic CAN handler

---
+++ can_tools.cpp
@@ -0,0 +1,112 @@
+#include "can_tools.hpp"
+
+canMan::canMan(can_controller target_can_controller, int target_baud) {
+  switch (target_can_controller) {
+  case TEENSY_CAN1:
+    init_flexcan_1(target_baud);
+
+    this->controller_has_new_msg = flexcan_1_has_new_msg;
+    this->send_controller_new_msg = flexcan_1_send_msg;
+    break;
+
+  case TEENSY_CAN2:
+    init_flexcan_2(target_baud);
+
+    this->controller_has_new_msg = flexcan_2_has_new_msg;
+    this->send_controller_new_msg = flexcan_2_send_msg;
+    break;
+
+  case TEENSY_CAN3:
+    init_flexcan_3(target_baud);
+
+    this->controller_has_new_msg = flexcan_3_has_new_msg;
+    this->send_controller_new_msg = flexcan_2_send_msg;
+    break;
+  }
+}
+
+// TODO: This makes me cry but template functions here make me want to die so
+#ifdef TEENSYDUINO
+FlexCAN_T4<CAN1, RX_SIZE_1024, TX_SIZE_1024> flexcan_1;
+FlexCAN_T4<CAN2, RX_SIZE_1024, TX_SIZE_1024> flexcan_2;
+FlexCAN_T4<CAN3, RX_SIZE_1024, TX_SIZE_1024> flexcan_3;
+
+CAN_message_t msg1;
+CAN_message_t msg2;
+CAN_message_t msg3;
+
+void init_flexcan_1(int target_baud) {
+  flexcan_1.begin();
+  flexcan_1.setBaudRate(target_baud);
+}
+void init_flexcan_2(int target_baud) {
+  flexcan_2.begin();
+  flexcan_2.setBaudRate(target_baud);
+}
+void init_flexcan_3(int target_baud) {
+  flexcan_3.begin();
+  flexcan_3.setBaudRate(target_baud);
+}
+
+bool flexcan_1_has_new_msg(can_message *message_out) {
+  if (flexcan_1.read(msg1)) {
+    message_out->id = msg1.id;
+    message_out->length = msg1.len;
+
+    if (msg1.len <= 8)
+      memcpy(message_out->buf.byte, msg1.buf, msg1.len);
+
+    return true;
+  } else {
+    return false;
+  }
+}
+bool flexcan_2_has_new_msg(can_message *message_out) {
+  if (flexcan_2.read(msg2)) {
+    message_out->id = msg2.id;
+    message_out->length = msg2.len;
+
+    if (msg2.len <= 8)
+      memcpy(message_out->buf.byte, msg2.buf, msg2.len);
+
+    return true;
+  } else {
+    return false;
+  }
+}
+bool flexcan_3_has_new_msg(can_message *message_out) {
+  if (flexcan_3.read(msg3)) {
+    message_out->id = msg3.id;
+    message_out->length = msg3.len;
+
+    if (msg3.len <= 8)
+      memcpy(message_out->buf.byte, msg3.buf, msg3.len);
+
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void flexcan_1_send_msg(can_message *message_out) {
+  msg1.id = message_out->id;
+  msg1.len = message_out->length;
+  memcpy(msg1.buf, message_out->buf.byte, message_out->length);
+
+  flexcan_1.write(msg1);
+}
+void flexcan_2_send_msg(can_message *message_out) {
+  msg2.id = message_out->id;
+  msg2.len = message_out->length;
+  memcpy(msg2.buf, message_out->buf.byte, message_out->length);
+
+  flexcan_2.write(msg2);
+}
+void flexcan_3_send_msg(can_message *message_out) {
+  msg3.id = message_out->id;
+  msg3.len = message_out->length;
+  memcpy(msg3.buf, message_out->buf.byte, message_out->length);
+
+  flexcan_3.write(msg3);
+}
+#endif
---
+++ can_tools.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <stdint.h>
+
+// TODO: Add in the MCP2515 CAN fella
+enum can_controller { TEENSY_CAN1 = 0, TEENSY_CAN2 = 1, TEENSY_CAN3 = 2 };
+
+// TODO: Maybe add more of the CAN message settings to this fella
+struct can_message {
+  uint16_t id;
+  uint8_t length;
+  union {
+    uint8_t byte[8];
+    uint64_t val;
+  } buf;
+};
+
+// TODO: Make this a template function type beat
+class canMan {
+private:
+  bool (*controller_has_new_msg)(can_message *);
+  void (*send_controller_new_msg)(can_message *);
+
+  can_message controller_message;
+
+public:
+  canMan(can_controller target_controller, int target_baud);
+
+  inline bool check_controller_message() {
+    return this->controller_has_new_msg(&controller_message);
+  }
+
+  inline can_message get_controller_message() {
+    return this->controller_message;
+  }
+
+  inline void send_controller_message(can_message out_msg) {
+    this->send_controller_new_msg(&out_msg);
+  }
+};
+
+// TODO: Sadge
+#ifdef TEENSYDUINO
+#include <FlexCAN_T4.h>
+
+void init_flexcan_1(int target_baud);
+void init_flexcan_2(int target_baud);
+void init_flexcan_3(int target_baud);
+
+bool flexcan_1_has_new_msg(can_message *message_out);
+bool flexcan_2_has_new_msg(can_message *message_out);
+bool flexcan_3_has_new_msg(can_message *message_out);
+
+void flexcan_1_send_msg(can_message *message_out);
+void flexcan_2_send_msg(can_message *message_out);
+void flexcan_3_send_msg(can_message *message_out);
+#endif
